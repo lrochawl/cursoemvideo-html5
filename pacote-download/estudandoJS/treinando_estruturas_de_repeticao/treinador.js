0ad7f806e7b685c17813e90a5f6bf90a88813773
Lucas Rocha <wltopos@gmail.com>
2020-07-27 20:16:51 -0300

    Estruturas de repetição

--- pacote-download/estudandoJS/treinando_estruturas_de_repeticao/treinador.js
+++ pacote-download/estudandoJS/treinando_estruturas_de_repeticao/treinador.js
@@ -1,9 +1,13 @@
 var c = 1
-do{
+do{//teste logico é feito no inicio
     console.log(`${c} Oi tudo bem?`)
     c++
-}while(c <= 6){
+}while(c <= 6){ //teste logico é feito no final
     
     console.log(`${c} Fim`)
 
 } 
+
+for(i = 0; i <= 10; i++){ // estrutura ja teste logico e variavel de incremento
+    console.log(`${i} Oi tudo bem?`)
+}
